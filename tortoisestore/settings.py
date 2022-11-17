0c38ed5be793719c76e2aa7eb323e2f94ab67757
Kipp AD <kippad@protonmail.com>
2022-11-17 18:42:42 +0000

    Set secret key

--- tortoisestore/settings.py
+++ tortoisestore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-41h+2t=-c-wojc2g^kv@%xqxe9yo#u=o_8qkbaohgq7_avxo&3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
